f7a47cd8b2c8fa546abf8e6b8dd7978353aae308
Leonardo Rodriguez Vega <vegaleandro757@gmail.com>
2023-10-28 21:51:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/archivomultimedia/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.archivomultimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView video = findViewById(R.id.videoView);
+        String videdo = "android.resource://"+getPackageName()+"/"+R.raw.nyancat;
+        Uri uri = Uri.parse(videdo);
+        video.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        video.setMediaController(mediaController);
+        mediaController.setAnchorView(video);
+    }
+}
